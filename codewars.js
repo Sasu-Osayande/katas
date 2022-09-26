231a9cc9335252ade31f9b94ab75ecee1d307185
Sasu Osayande <osayande.sasu@outlook.com>
2022-09-26 10:03:03 -0400

    Solve string reverse

--- codewars.js
+++ codewars.js
@@ -1,1 +1,19 @@
-// Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+/*
+1. Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present. */
+
+function stringReverse(str) {
+  let newString = "";
+  // Get array of words to loop through each individual word
+  let words = str.split(" ");
+  for (let i = 0; i < words.length; i++) {
+    // If less than 5, return word, else reverse word
+    if (words[i].length <= 4) {
+      newString += words[i] + " ";
+    } else {
+      newString += words[i].split("").reverse().join("") + " ";
+    }
+  }
+  return newString;
+}
+
+console.log(stringReverse("hello I am doing well today"));
