093d2122c104e6c5d1785a9d836dd461715c7a51
Sasu Osayande <osayande.sasu@outlook.com>
2022-09-27 23:33:29 -0400

    Add descendingOrder

--- codewars.js
+++ codewars.js
@@ -23,46 +23,57 @@
 
 Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation. */
 
-function isPangram(string) {
-  const alphabet = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-  ];
-  // Separate string into an array
-  let newString = string.toLowerCase().split("");
-  // Iterate through alphabet and return false if string doesn't include letter
-  for (const letter of alphabet) {
-    for (let i = 0; i < newString.length; i++) {
-      if (!newString.includes(letter)) {
-        return false;
-      }
-    }
-  }
-  return true;
-}
+// function isPangram(string) {
+//   const alphabet = [
+//     "a",
+//     "b",
+//     "c",
+//     "d",
+//     "e",
+//     "f",
+//     "g",
+//     "h",
+//     "i",
+//     "j",
+//     "k",
+//     "l",
+//     "m",
+//     "n",
+//     "o",
+//     "p",
+//     "q",
+//     "r",
+//     "s",
+//     "t",
+//     "u",
+//     "v",
+//     "w",
+//     "x",
+//     "y",
+//     "z",
+//   ];
+//   // Separate string into an array
+//   let newString = string.toLowerCase().split("");
+//   // Iterate through alphabet and return false if string doesn't include letter
+//   for (const letter of alphabet) {
+//     for (let i = 0; i < newString.length; i++) {
+//       if (!newString.includes(letter)) {
+//         return false;
+//       }
+//     }
+//   }
+//   return true;
+// }
 
-console.log(isPangram("The quick brown fox jumps over the lazy dog"));
+// console.log(isPangram("The quick brown fox jumps over the lazy dog"));
+
+/*
+3. Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number. */
+
+// function descendingOrder(n) {
+//   let numberArr = n.split(" ")
+//   let emptyArr = [];
+
+//   for (let i = 0; i < n.length; i++) {
+//   }
+// }
